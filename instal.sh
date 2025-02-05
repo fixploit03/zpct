bb3acd04176a91d07cd5bfc1f29fad4c739f1bb2
fixploit03 <164528883+fixploit03@users.noreply.github.com>
2025-02-06 02:34:52 +0700

    Add files via upload

--- instal.sh
+++ instal.sh
@@ -165,7 +165,7 @@ function cek_status_instal_zpct(){
 
 	if [[ "${#gagal[@]}" -eq 0 ]]; then
 		echo "[+] zpct berhasil diinstal."
-		echo "[+] Untuk menjalankannya ketikkan 'zpct'"
+		echo "[+] Untuk menjalankannya ketikkan 'zpct --help'"
 		exit 0
 	else
 		echo "[-] zpct gagal diinstal."
